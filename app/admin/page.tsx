e90fdbea5220764f89c244c0582bb7a494606317
arzerra <ianclark1506@gmail.com>
2025-09-25 00:35:47 +0800

    adminPage

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { supabase } from "@/lib/supabase";
 import { useRouter } from "next/navigation";
 import LogoutButton from "../components/LogoutButton";
+import AddPatient from "./add-patient/page";
 
 export default function AdminPage() {
   const router = useRouter();
@@ -29,36 +30,13 @@ export default function AdminPage() {
   if (loading) return null;
 
   return (
-    <>
     
+    <>
     <div className="flex justify-between p-4">
       <div className="flex">Welcome</div>
       <div className="flex"><LogoutButton /></div>
     </div>
-
-    <div className="flex flex-col justify-center items-center">
-      <h1>Add New Patient</h1>
-      <div className="flex flex-col items-start justify-center">
-        <div className="mt-5">
-          <label>Name: </label>
-          <input type="text" placeholder="Patient's Name..."/>
-        </div>
-        <div className="mt-5">
-          <label>Contact Number: </label>
-          <input type="tel" placeholder="Patient's Name..."/>
-        </div>
-        <div className="mt-5">
-          <label>Birthday: </label>
-          <input type="text" placeholder="Patient's Name..."/>
-        </div>
-        <div className="mt-5">
-          <label>Gender: </label>
-          <input type="text" placeholder="Patient's Name..."/>
-        </div>
-      </div>
-        <button className="flex cursor-pointer items-center justify-center p-5">Add</button>
-    </div>
-    
+    <AddPatient/>
     </>
   );
 }
