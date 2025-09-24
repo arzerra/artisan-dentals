82344887802bcb8b80e227eaa2800de9fd811f0b
arzerra <ianclark1506@gmail.com>
2025-09-24 18:08:55 +0800

    admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -36,6 +36,28 @@ export default function AdminPage() {
       <div className="flex"><LogoutButton /></div>
     </div>
 
+    <div className="flex flex-col justify-center items-center">
+      <h1>Add New Patient</h1>
+      <div className="flex flex-col items-start justify-center">
+        <div className="mt-5">
+          <label>Name: </label>
+          <input type="text" placeholder="Patient's Name..."/>
+        </div>
+        <div className="mt-5">
+          <label>Contact Number: </label>
+          <input type="tel" placeholder="Patient's Name..."/>
+        </div>
+        <div className="mt-5">
+          <label>Birthday: </label>
+          <input type="text" placeholder="Patient's Name..."/>
+        </div>
+        <div className="mt-5">
+          <label>Gender: </label>
+          <input type="text" placeholder="Patient's Name..."/>
+        </div>
+      </div>
+        <button className="flex cursor-pointer items-center justify-center p-5">Add</button>
+    </div>
     
     </>
   );
