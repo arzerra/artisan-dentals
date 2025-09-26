ccf6bca4687e9c7305160a6368069c58213c7c3f
arzerra <ianclark1506@gmail.com>
2025-09-26 19:45:11 +0800

    page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -40,6 +40,21 @@ export default function AdminPage() {
       <div className="flex"><LogoutButton /></div>
     </div>
     <AddPatient/>
+    <div className="flex justify-between p-4">
+      <div className="flex">Welcome</div>
+      <div className="flex"><LogoutButton /></div>
+    </div>
+    <AddPatient/>
+    <div className="flex justify-between p-4">
+      <div className="flex">Welcome</div>
+      <div className="flex"><LogoutButton /></div>
+    </div>
+    <AddPatient/>
+    <div className="flex justify-between p-4">
+      <div className="flex">Welcome</div>
+      <div className="flex"><LogoutButton /></div>
+    </div>
+    <AddPatient/>
     </>
   );
 }
