795d86d6b12ebcf4b5d0e1e503cf1d345432ec46
Arzerra <ianclrk.canete@gmail.com>
2025-10-01 12:09:03 +0800

    testing if force-dynamic is removed

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-export const dynamic = "force-dynamic";
 import { useEffect, useState } from "react";
 import type { User } from "@supabase/supabase-js";
 import { supabase } from "@/lib/supabase";
